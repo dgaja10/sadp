34a87fcc53be8931ff325c13bfa05b856559b1f5
dgaja10 <dgaja10@gmail.com>
2025-11-16 22:30:00 +0530

    Update slip5.java

--- slip 5/slip5.java
+++ slip 5/slip5.java
@@ -1,25 +1,37 @@
 import java.util.*;
-class EnumerationIterator implements Iterator {
-Enumeration enumeration;
-public EnumerationIterator(Enumeration enumeration) {
-this.enumeration = enumeration;
+
+class EnumerationIterator<T> implements Iterator<T> {
+    private final Enumeration<T> enumeration;
+
+    public EnumerationIterator(Enumeration<T> enumeration) {
+        this.enumeration = enumeration;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return enumeration.hasMoreElements();
+    }
+
+    @Override
+    public T next() {
+        return enumeration.nextElement();
+    }
+
+    @Override
+    public void remove() {
+        throw new UnsupportedOperationException();
+    }
 }
-public boolean hasNext() {
-return enumeration.hasMoreElements();
-}
-public Object next() {
-return enumeration.nextElement();
-}
-public void remove() {
-throw new UnsupportedOperationException();
-}
-}
+
 public class Main {
-public static void main (String args[]) {
-Vector v = new Vector(Arrays.asList(args));
-Iterator iterator = new EnumerationIterator(v.elements());
-while (iterator.hasNext()) {
-System.out.println(iterator.next());
-}
-}
+    public static void main(String[] args) {
+
+        Vector<String> v = new Vector<>(Arrays.asList(args));
+
+        Iterator<String> iterator = new EnumerationIterator<>(v.elements());
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
 }
