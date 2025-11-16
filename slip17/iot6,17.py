90dde262403a980e15dbaf22a5faaa3f81202250
dgaja10 <dgaja10@gmail.com>
2025-11-16 19:59:45 +0530

    Update iot6,17.py

--- slip17/iot6,17.py
+++ slip17/iot6,17.py
@@ -11,4 +11,15 @@ Serial.begin(9600);
 dht.begin();
 pinMode(dhtSensorPin, INPUT);
 delay(2000);
+
+}
+void loop ()
+{
+    t = dht.readTemperature();
+    Serial.print("Temperature=");
+    Serial.println(t);
+    h = dht.readHumidity();
+    Serial.print("Humidity=");
+    Serial.println(h);
+    delay(500);
 }
